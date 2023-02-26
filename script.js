ccccc99091ddc462134bdd52e735a77ed180e79d
Roderen <mikilikala@gmail.com>
2023-02-26 22:33:51 +0200

    init

---
+++ script.js
@@ -0,0 +1,19 @@
+// Task #1
+let admin;
+let name = "Джон";
+
+admin = name;
+
+alert(admin);
+
+
+
+// Task #2
+let ourPlanet;
+let currentUser;
+
+
+
+// Task #3
+let val = '10' + 10;
+console.log(typeof(val));
